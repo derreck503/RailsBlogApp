6e051250e6e663e72e6732a5fb011870e2940c35
derreck503 <derreck503@gmail.com>
2017-06-29 00:45:15 -0700

    Post can now be edited

--- SimpleBlog/app/controllers/posts_controller.rb
+++ SimpleBlog/app/controllers/posts_controller.rb
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
     end
 
     def new
+        @post = Post.new
     end
     
     def create 
@@ -17,9 +18,28 @@ class PostsController < ApplicationController
         # post = Post.new(params[:post]) This is forbiden because parameters must be specified
         @post = Post.new(post_params)
 
-        @post.save
-        redirect_to @post
+        if(@post.save)
+            redirect_to @post
+        else
+            render 'new'
+        end
+        
     end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+
+        if(@post.update(post_params))
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+    
 
     # Define input parameters
     private def post_params
--- SimpleBlog/config/routes.rb
+++ SimpleBlog/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Setting root of site 
-  root 'posts#index'
+  root 'posts#index', as: 'home'
 
   # Setting regular get route
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about', as: 'about'
   
   # Generate all routes for controller. Run 'rake routes' to view routes
   resources :posts
