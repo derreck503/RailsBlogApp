924c066029aca509ce39b480692159d6476030e4
derreck503 <derreck503@gmail.com>
2017-06-28 22:20:31 -0700

    Get about and root pages up

--- SimpleBlog/config/routes.rb
+++ SimpleBlog/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Setting domain root 
+  root 'posts#index'
+
+  # Setting url path for about page
+  get 'about' => 'pages#about'
+
+  
 end
